a27cb58dfd6bc6c4adf6b8ff9787dd396a73ee42
kwanele <your-email@example.com>
2025-04-04 02:29:32 +0200

    newly

---
+++ monday.java
@@ -0,0 +1,12 @@
+public class new {
+    public static void main(Sting []args){
+
+        int a,b,c;
+
+        a=5;
+        b=2;
+
+        c =a*b;
+        System.out.println(c);
+    }
+}
---
+++ new.java
@@ -0,0 +1,12 @@
+public class monday{
+    public static void main(Sting []args){
+
+        int a,b,c;
+
+        a=5;
+        b=2;
+
+        c =a*b;
+        System.out.println(c);
+    }
+}
